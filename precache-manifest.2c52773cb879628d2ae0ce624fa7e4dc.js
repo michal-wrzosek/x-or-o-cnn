4a45209a8b61cc5e34fcc5ee58f9c8b4f1942bb9
Michał Wrzosek <michal@wrzosek.pl>
2019-03-24 23:01:20 +0100

    Updates

---
+++ precache-manifest.2c52773cb879628d2ae0ce624fa7e4dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f6257ae53853abda728",
+    "url": "/x-or-o-cnn/static/js/runtime~main.f5899c71.js"
+  },
+  {
+    "revision": "f21e707a5c5b60b1cfae",
+    "url": "/x-or-o-cnn/static/js/main.755ff957.chunk.js"
+  },
+  {
+    "revision": "39bac347827072c202f4",
+    "url": "/x-or-o-cnn/static/js/2.08017400.chunk.js"
+  },
+  {
+    "revision": "f21e707a5c5b60b1cfae",
+    "url": "/x-or-o-cnn/static/css/main.f3360bb3.chunk.css"
+  },
+  {
+    "revision": "7241750253ba8cbad903e69b0f2ffb56",
+    "url": "/x-or-o-cnn/index.html"
+  }
+];
